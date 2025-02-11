30f1866d7c1489d20cfa9757b3c04123617f83fa
songle <951540966@qq.com>
2025-02-11 18:33:18 +0800

    fix: code

--- types/package/event/index.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class EventBindingThis {
-    protected bindEventThis(functionNameList: string[], args?: {
-        [key: string]: any;
-    }): this;
-}
--- types/package/view.d.ts
+++
@@ -1,6 +0,0 @@
-import { default as Gantt } from '.';
-export declare class View {
-    gantt: Gantt;
-    constructor(gantt: Gantt);
-    scrollTo(): void;
-}
